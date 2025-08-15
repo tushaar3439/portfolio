0455f8207debee4296a26157e9400a5592eae156
tushaar3439 <tuusshhar@gmail.com>
2025-08-15 23:55:34 +0530

    changes test 1

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ import { HiOutlineDesktopComputer } from "react-icons/hi";
 import { HiMiniDevicePhoneMobile } from "react-icons/hi2";
 import { GrHostMaintenance } from "react-icons/gr";
 
+import image from './assets/profile2.jpg';
+
 import { useInView } from 'react-intersection-observer';
 
 
