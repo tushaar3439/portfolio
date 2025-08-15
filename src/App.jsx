61c30c119e76204b9cb6c1873a3bf14a497a6a59
tushaar3439 <tuusshhar@gmail.com>
2025-08-16 00:14:59 +0530

    fix profile

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ import { HiOutlineDesktopComputer } from "react-icons/hi";
 import { HiMiniDevicePhoneMobile } from "react-icons/hi2";
 import { GrHostMaintenance } from "react-icons/gr";
 
-import profile2 from './assets/profile2.jpg';
 
 import { useInView } from 'react-intersection-observer';
 
